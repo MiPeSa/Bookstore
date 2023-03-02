cc50b89279f6d40717d0d1193e03bc5391b686bd
MiPeSa <bgu109@myy.haaga-helia.fi>
2023-03-02 20:53:25 +0200

    C5 2. Enabled Spring Data REST. Added edit, delete, add methods

--- src/main/java/k23BE/Bookstore/web/RestBookController.java
+++ src/main/java/k23BE/Bookstore/web/RestBookController.java
@@ -3,9 +3,12 @@ package k23BE.Bookstore.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import k23BE.Bookstore.domain.Book;
@@ -32,7 +35,30 @@ public class RestBookController {
 		return bookRepository.findAll();
 	}
 
+	// add new car
+	@PostMapping("books")
+	Book newBook(@RequestBody Book newBook) {
+		log.info("save new car " + newBook);
+		return bookRepository.save(newBook);
+	}
+
+	// edit car information
+	@PutMapping("/books/{id}")
+	Book editBook(@RequestBody Book editedBook, @PathVariable Long id) {
+		log.info("edit car " + editedBook);
+		editedBook.setId(id);
+		return bookRepository.save(editedBook);
+	}
+
 	
+	// delete car
+	@DeleteMapping("/books/{id}")
+	public Iterable<Book> deleteCar(@PathVariable Long id) {
+		log.info("delete book, id = " + id);
+		bookRepository.deleteById(id);
+		return bookRepository.findAll();
+	}
+
 	// find one car and return it
 	@GetMapping("/books/{id}")
 	Optional<Book> getCar(@PathVariable Long id) {
